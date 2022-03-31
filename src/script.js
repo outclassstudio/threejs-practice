3ce10d0f01025dd9a105478698417e5078f6238f
outclassstudio <mlimshs@gmail.com>
2022-03-31 16:56:56 +0900

    feat : mesh-move etc

--- src/script.js
+++ src/script.js
@@ -4,6 +4,9 @@ import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+//URL
+const rootUrl = "http://localhost:8081/static";
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -13,43 +16,48 @@ const scene = new THREE.Scene();
 //Plane
 const planeSize = 250;
 const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-const planeMat = new THREE.MeshPhongMaterial({
-  shininess: 20,
-  // emissive: "black",
-  // roughness: 1,
-  // color: 0x9acd32,
+const planeMat = new THREE.MeshStandardMaterial({
+  roughness: 0.6,
+  metalness: 0.5,
   color: 0x006400,
   side: THREE.DoubleSide,
 });
 const plane = new THREE.Mesh(planeGeo, planeMat);
+plane.receiveShadow = true;
 plane.rotation.x = Math.PI * -0.5;
 plane.position.set(0, -10, 0);
 scene.add(plane);
 
 //Sky
-{
-  const loader = new THREE.CubeTextureLoader();
-  const texture = loader.load([
-    "http://localhost:8081/static/afterrain/afterrain_rt.jpg",
-    "http://localhost:8081/static/afterrain/afterrain_lf.jpg",
-    "http://localhost:8081/static/afterrain/afterrain_up.jpg",
-    "http://localhost:8081/static/afterrain/afterrain_dn.jpg",
-    "http://localhost:8081/static/afterrain/afterrain_bk.jpg",
-    "http://localhost:8081/static/afterrain/afterrain_ft.jpg",
-  ]);
-  scene.background = texture;
-}
+const skyLoader = new THREE.CubeTextureLoader();
+const skyTexture = skyLoader.load([
+  `${rootUrl}/afterrain/afterrain_rt.jpg`,
+  `${rootUrl}/afterrain/afterrain_lf.jpg`,
+  `${rootUrl}/afterrain/afterrain_up.jpg`,
+  `${rootUrl}/afterrain/afterrain_dn.jpg`,
+  `${rootUrl}/afterrain/afterrain_bk.jpg`,
+  `${rootUrl}/afterrain/afterrain_ft.jpg`,
+]);
+scene.background = skyTexture;
 
 //Object
 const objects = [];
 
+//*기본박스
+const textureLoader = new THREE.TextureLoader();
 const geometry = new THREE.BoxGeometry(20, 20, 20);
-// const matTexture = loader.load()
-const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+const material = new THREE.MeshStandardMaterial({
+  roughness: 0.8,
+  metalness: 0.8,
+  map: textureLoader.load(`${rootUrl}/textures/wall_copy.jpg`),
+});
 const mesh = new THREE.Mesh(geometry, material);
+mesh.receiveShadow = true;
+mesh.castShadow = true;
 mesh.position.set(0, 0, 0);
 scene.add(mesh);
 
+//*오브젝트 불러오는 진행중 실행
 const onProgress = function (xhr) {
   if (xhr.lengthComputable) {
     const percentComplete = (xhr.loaded / xhr.total) * 100;
@@ -57,210 +65,70 @@ const onProgress = function (xhr) {
   }
 };
 
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
+//*오브젝트 생성
+for (let i = 0; i < 7; i++) {
+  const positionArr = [
+    [20, 5],
+    [-50, 70],
+    [-80, 25],
+    [70, 10],
+    [10, 95],
+    [-90, -80],
+    [0, -60],
+  ];
 
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(20, -10, 5);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
+  new MTLLoader()
+    .setPath(`${rootUrl}/models/obj/male02/`)
+    .load("male02.mtl", function (materials) {
+      materials.preload();
 
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(-20, -10, 1);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(-50, -10, 25);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(60, -10, 15);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(10, -10, 65);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(-90, -10, -80);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-new MTLLoader()
-  .setPath("http://localhost:8081/static/models/obj/male02/")
-  .load("male02.mtl", function (materials) {
-    materials.preload();
-
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath("http://localhost:8081//static/models/obj/male02/")
-      .load(
-        "male02.obj",
-        function (object) {
-          object.position.set(0, -10, -60);
-          object.scale.set(0.1, 0.1, 0.1);
-          scene.add(object);
-          objects.push(object);
-        },
-        onProgress
-      );
-  });
-
-/**
- * Sizes
- */
-// const sizes = {
-//   width: 800,
-//   height: 600,
-// };
+      new OBJLoader()
+        .setMaterials(materials)
+        .setPath(`${rootUrl}/models/obj/male02/`)
+        .load(
+          "male02.obj",
+          function (object) {
+            object.position.set(positionArr[i][0], -10, positionArr[i][1]);
+            object.scale.set(0.1, 0.1, 0.1);
+            //?오브젝트에 그림자 넣기
+            object.traverse(function (child) {
+              child.receiveShadow = true;
+              child.castShadow = true;
+            });
+            scene.add(object);
+            objects.push(object);
+          },
+          onProgress
+        );
+    });
+}
 
 //Camera
+//*메인카메라(조건에따라 업데이트)
 let camera;
 
+//*카메라기본설정
 const fov = 75;
 const near = 0.1;
 const far = 1500;
 const aspect = 800 / 600;
 
+//*첫번째카메라(기본)
 const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera1.position.set(0, 170, 130);
+camera1.position.set(0, 150, 160);
 const controls = new OrbitControls(camera1, canvas);
 controls.target.set(0, 0, 0);
 controls.update();
 
-let LEFT = 37,
-  RIGHT = 39,
-  UP = 38,
-  DOWN = 40;
-// let add = 0.2;
-
+//*두번째카메라
 const camera2 = new THREE.PerspectiveCamera(45, aspect, near, far);
 camera2.position.set(0, 10, 0);
-// const controls2 = new OrbitControls(camera2, canvas);
-// controls2.target.set(0, 100, -1000);
-// controls2.update();
 
 camera = camera1;
 scene.add(camera);
 
-// let keyCodeOn = function (e) {
-//   console.log(e);
-//   if (e.keyCode == LEFT) {
-//     add = -0.5;
-//     mesh.position.x += add;
-//     camera2.position.x += add;
-//   } else if (e.keyCode == RIGHT) {
-//     add = 0.5;
-//     mesh.position.x += add;
-//     camera2.position.x += add;
-//   } else if (e.keyCode == DOWN) {
-//     add = 0.5;
-//     mesh.position.z += add;
-//     camera2.position.z += add;
-//     // scene.rotation.x += add;
-//   } else if (e.keyCode == UP) {
-//     // scene.rotation.x -= add;
-//     add = -0.5;
-//     mesh.position.z += add;
-//     camera2.position.z += add;
-//   } else {
-//     return;
-//   }
-// };
-
+//*시점전환 버튼으로 카메라 전환
 const btn = document.querySelector("button");
-
 btn.onclick = function cameraChange() {
   if (camera === camera2) {
     camera = camera1;
@@ -269,18 +137,77 @@ btn.onclick = function cameraChange() {
   }
 };
 
+//*C키로 카메라 전환
+function cameraChangeByKey(e) {
+  if (e.keyCode == 67) {
+    if (camera === camera2) {
+      camera = camera1;
+    } else {
+      camera = camera2;
+    }
+  }
+}
+document.addEventListener("keydown", cameraChangeByKey, false);
+
 // Lights
+//*기본조명(자연광)
 const color = 0xffffff;
-const intensity = 0.3;
+const intensity = 2;
 const baseLight = new THREE.AmbientLight(color, intensity);
 scene.add(baseLight);
 
-const dLIntensity = 2;
+//*기본조명2(햇빛)
+//?조명궤도
+const ligthOrbit = new THREE.Object3D();
+scene.add(ligthOrbit);
+
+const dLIntensity = 1;
 const directLight = new THREE.DirectionalLight(color, dLIntensity);
-directLight.position.set(40, 200, 70);
+directLight.castShadow = true;
+directLight.position.set(100, 150, 100);
 directLight.target.position.set(0, 0, 0);
-scene.add(directLight);
+directLight.shadow.mapSize = new THREE.Vector2(2000, 2000);
+directLight.shadow.radius = 2;
+directLight.shadow.camera.top = directLight.shadow.camera.right = 150;
+directLight.shadow.camera.bottom = directLight.shadow.camera.left = -150;
+
+// scene.add(directLight);
+ligthOrbit.add(directLight);
 scene.add(directLight.target);
+
+//?다이렉트 조명 헬퍼
+const helper = new THREE.DirectionalLightHelper(directLight);
+scene.add(helper);
+
+//?다이렉트 조명 그림자 카메라 헬퍼
+const cameraHelper = new THREE.CameraHelper(directLight.shadow.camera);
+scene.add(cameraHelper);
+
+//*보조조명(귀퉁이에 4개추가, plane에 안비추도록)
+for (let i = 0; i < 4; i++) {
+  const color = 0xffffff;
+  const intensity = 1;
+  const position = [
+    [250, 250],
+    [-250, 250],
+    [250, -250],
+    [-250, -250],
+  ];
+  const directLights = new THREE.DirectionalLight(color, intensity);
+  directLights.position.set(position[i][0], 0, position[i][1]);
+  directLights.target.position.set(
+    position[i][0] * -1,
+    10,
+    position[i][1] * -1
+  );
+
+  //?보조조명 헬퍼
+  const auxHelper = new THREE.DirectionalLightHelper(directLights);
+  // scene.add(auxHelper);
+
+  scene.add(directLights);
+  scene.add(directLights.target);
+}
 
 //Controls
 function resizeRendererToDisplaySize(renderer) {
@@ -295,12 +222,47 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
+let add = 0.003;
+
+//렌더함수
 function render(time) {
   time = time * 0.005;
+
+  ligthOrbit.rotation.y = time * 0.01;
 
   objects.forEach((obj) => {
     obj.rotation.y = time;
   });
+
+  let keyCodeOn = function (e) {
+    let { x, y, z } = controls.target;
+
+    if (e.keyCode == 37) {
+      add = -0.003;
+      mesh.position.x += add;
+      controls.target.set(x + add, y, z);
+      camera2.position.x += add;
+    } else if (e.keyCode == 39) {
+      add = 0.003;
+      mesh.position.x += add;
+      controls.target.set(x + add, y, z);
+      camera2.position.x += add;
+    } else if (e.keyCode == 40) {
+      add = 0.003;
+      mesh.position.z += add;
+      controls.target.set(x, y, z + add);
+      camera2.position.z += add;
+    } else if (e.keyCode == 38) {
+      add = -0.003;
+      mesh.position.z += add;
+      controls.target.set(x, y, z + add);
+      camera2.position.z += add;
+    } else {
+      return;
+    }
+    controls.update();
+  };
+  document.addEventListener("keydown", keyCodeOn, false);
 
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
@@ -312,58 +274,18 @@ function render(time) {
   camera.aspect = canvas.clientWidth / canvas.clientHeight;
   camera.updateProjectionMatrix();
 
-  // keyCodeOn();
-
-  let add = 0.005;
-  let keyCodeOn = function (e) {
-    console.log(controls.target);
-    let x = controls.target.x;
-    let y = controls.target.y;
-    let z = controls.target.z;
-    if (e.keyCode == LEFT) {
-      add = -0.003;
-      mesh.position.x += add;
-      controls.target.set(x + add, y, z);
-      camera2.position.x += add;
-    } else if (e.keyCode == RIGHT) {
-      add = 0.003;
-      mesh.position.x += add;
-      controls.target.set(x + add, y, z);
-      camera2.position.x += add;
-    } else if (e.keyCode == DOWN) {
-      add = 0.003;
-      mesh.position.z += add;
-      controls.target.set(x, y, z + add);
-      camera2.position.z += add;
-      // scene.rotation.x += add;
-    } else if (e.keyCode == UP) {
-      // scene.rotation.x -= add;
-      add = -0.003;
-      mesh.position.z += add;
-      controls.target.set(x, y, z + add);
-      camera2.position.z += add;
-    } else {
-      return;
-    }
-    controls.update();
-  };
-
-  document.addEventListener("keydown", keyCodeOn, false);
-
   renderer.render(scene, camera);
 
   requestAnimationFrame(render);
 }
 requestAnimationFrame(render);
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   alpha: true,
+  antialias: true,
 });
-// renderer.setSize(sizes.width, sizes.height);
-// document.addEventListener("keydown", keyCodeOn, false);
+renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.render(scene, camera);
